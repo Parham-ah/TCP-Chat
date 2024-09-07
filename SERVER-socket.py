39cc09e2e1148a6f6a303409bec22398631094b1
Parham-ah <ahmadianparham04@gmail.com>
2024-09-07 20:37:57 +0330

    Update SERVER-socket.py

--- SERVER-socket.py
+++ SERVER-socket.py
@@ -1,23 +1,29 @@
+from encodings import utf_8
 import socket
+import threading
 
-IP='127.0.0.1'
-PORT=1995
+HOST = '127.0.0.1'
+PORT = 61324
 
-#server code
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((IP, PORT))
-    s.listen()
-    print(f'server is ready to listen {IP}:{PORT}...')
-    conn, addr = s.accept()
-
+def handle_client(conn, addr):
+    print(f"new client connected : {addr}")
     with conn:
-        print(f'connected by {addr}')
-        data = conn.recv(1024)
-        print('recived message from client: ', data.decode('utf-8'))
-        conn.sendall(b'Hello, Client')
-    
+        while True:
+            data = conn.recv(1024)
+            print(f'Recived message from {addr}: {data.decode("utf-8")}')
+            message = input('Enter your message : ')
+            conn.sendall(message.encode())
 
+def start_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        print(f'server is listening on {HOST}:{PORT}')  
+        while True:
+            conn, addr = s.accept()
+            threading.Thread(target = handle_client, args= (conn, addr)).start() 
+
+start_server()                 
 
 
 
